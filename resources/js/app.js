388ce51f094a55b3c9704e608632b9e48eefeed2
mikseros <mmadynski@gmail.com>
2022-02-04 10:18:11 +0100

    Initial commit; Project starting.

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+mix.disableNotifications();
+
+const app = new Vue({
+    el: '#app',
+});
