4463ba64156d13e89a4ec6e0027607ea98058231
mikseros <mmadynski@gmail.com>
2022-02-04 14:55:40 +0100

    Api Routes created & checked.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,55 @@
 <?php
 
+use App\Http\Resources\TodayTaskResource;
+use App\Http\Resources\UpcomingResource;
+use App\Models\Upcoming;
+use App\Models\Today;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//** Upcoming Task */
+
+// Get all the upcoming task
+Route::get("/upcoming", function() {
+    $upcoming = Upcoming::all();
+    return UpcomingResource::collection($upcoming);
+});
+
+// Add a new task
+Route::post('/upcoming', function(Request $request){
+    return Upcoming::create([
+        'title' => $request->title,
+        'taskId' => $request->taskId,
+        'waiting' => $request->waiting
+    ]);
+});
+
+// Delete upcoming task
+Route::delete('/upcoming/{taskId}', function($taskId) {
+    DB::table('upcomings')->where('taskId', $taskId)->delete();
+
+    return 204;
+});
+
+//** Today Task */
+
+Route::post('/dailytask', function(Request $request) {
+    return Today::create([
+        'id' => $request->id,
+        'title' => $request->title,
+        'taskId' => $request->taskId
+    ]);
+});
+
+// Delete Today Task
+Route::delete('/dailytask/{taskId}', function($taskId){
+    DB::table('todays')->where('taskId', $taskId)->delete();
+
+    return 204;
 });
